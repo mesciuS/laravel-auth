3ec6b0f4785a1d99ae548547841a81ef0416f849
mesciuS <giovadev@gmail.com>
2023-05-21 19:10:25 +0200

    feat: seeder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,10 @@
             </svg>
         </div>
         <h1 class="display-5 fw-bold">
-            Welcome to Laravel+Bootstrap 5
+            Welcome to GiovanniLand
         </h1>
 
-        <p class="col-md-8 fs-4">This a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 10.x</p>
-        <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentation</a>
+        <p class="col-md-8 fs-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum earum ex minus, quae impedit suscipit, accusamus at, consectetur reiciendis modi illum veritatis nobis officia consequuntur! Obcaecati molestias nihil corrupti architecto?</p>
     </div>
 </div>
 
